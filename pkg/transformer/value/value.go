f866510468851c1ee53893ca32cfc6f24d36da2b
vitrevance <64753385+vitrevance@users.noreply.github.com>
2025-09-15 16:22:12 +0300

    Create value.go

---
+++ pkg/transformer/value/value.go
@@ -0,0 +1,23 @@
+package json
+
+import (
+	"github.com/vitrevance/api-exporter/pkg/transformer"
+	"gopkg.in/yaml.v3"
+)
+
+type valueTransformer struct {
+	Value any `yaml:"value"`
+}
+
+func init() {
+	transformer.RegisterTransformerFactory("value", transformer.TransformerFactoryFunc(func(value *yaml.Node) (transformer.Transformer, error) {
+		t := &valueTransformer{}
+		err := value.Decode(t)
+		return t, err
+	}))
+}
+
+func (this *valueTransformer) Transform(ctx *transformer.TransformationContext) error {
+	ctx.Result = this.Value
+	return nil
+}
